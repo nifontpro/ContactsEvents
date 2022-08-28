c8e60e59fcd9ec5d8be95a52261abde10adb1d1d
Nifont <nifontbus@yandex.ru>
2022-08-28 21:41:20 +0300

    7.31 Перед добавлением аннимации переходов

--- buildSrc/src/main/java/Build.kt
+++ buildSrc/src/main/java/Build.kt
@@ -1,10 +1,9 @@
 object Build {
 /*
-    private const val androidBuildToolsVersion = "7.1.2"
+    private const val androidBuildToolsVersion = "7.2.2"
     const val androidBuildTools = "com.android.tools.build:gradle:$androidBuildToolsVersion"
 */
 
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
-
     const val hiltAndroidGradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:${DaggerHilt.version}"
 }
